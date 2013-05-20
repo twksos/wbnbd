8b5101700dc8f11f3e0108aee8e13be2ac5bf19b
Wei Guangcheng <twksos@qq.com>
2013-05-20 20:16:05 +0800

    move the structure and use zclip for clipboard.

--- js/animate.js
+++ js/animate.js
@@ -10,9 +10,10 @@ window.requestAnimFrame = (function(callback){
 })();
 
 function animateStart(){
-  window.setTimeout(requestAnimFrame(animateType), 1000);
+  window.setTimeout(animateType, 1000);
 }
 function animateType() {
+  $('#nv').html('只要输入“'+ getHash('q') + '”');
   var str = getHash('q')
   var typed = $("#kw").attr("value");
   var toBeType = str[typed.length];
@@ -23,32 +24,44 @@ function animateType() {
   $("#kw").attr("value", typed + toBeType);
   requestAnimFrame(animateType);
 }
+function redirect(){
+  window.location.href = "http://www.baidu.com/s?wd=" + $('#kw').attr('value') + "&rsv_bp=0&rsv_spt=3";
+}
 function clickButton(){
   location.hash = '#click=true' 
-  $("#su").addClass('s_btn_h').delay(300);
-  window.location.href = "http://www.baidu.com/s?wd=" + $('#kw').attr('value') + "&rsv_bp=0&rsv_spt=3";
+  $('#nv').html('再按下这里，简单吧。');
+  $("#su").addClass('s_btn_h');
+  window.setTimeout(redirect, 1000);
 }
 function animateMoveToButton(){
   var dest = $("#su").offset();
-  $("#cursor").css("background", "url(mouse_arrow_windows_aero.png)");
+  $("#cursor").css("background", "url(img/mouse_arrow_windows_aero.png)");
   $("#cursor").animate({ left: dest.left + 45, top: dest.top + 16 }, 1000 , "linear", clickButton);
 }
 function typeStart(){
   var start = $("#cursor").offset();
-  $("#cursor").css("background", "url(mouse_text_windows_aero.png)");
+  $("#cursor").css("background", "url(img/mouse_text_windows_aero.png)");
   $("#cursor").animate({ left: start.left + 4, top: start.top + 4}, 100 , "linear", animateStart);
 }
 		
 window.onload = function(){
   if(getHash('q') === null) { 
+    $('#nv').html('连这个都不知道？教他们百度吧！');
     if(location.hash === '#click=true' ) $('#after-ad').show();
     $('#su').click(function() {
       var url = location.protocol + '//' + location.host + '/#q=' + encodeURIComponent($('#kw').attr('value'));
-      $('#nv').html('请分享: ' + url );
+      $('#nv').html('请分享: <input value="' + url + '" /> <a class="copy">复制链接</a>');
+      $('#nv a').zclip({
+        path: 'js/ZeroClipboard.swf',
+        copy: function(){return $('#nv input').val();},
+        setHandCursor: true,
+        afterCopy: function(){ $('.copy').text('重新复制')}
+      });
     });
   } else {
+    $('#nv').html('想知道“'+ getHash('q') + '”是什么？');
     var dest = $(".s_ipt_wr").offset();
-    $("#cursor").css("background", "url(mouse_arrow_windows_aero.png)");
+    $("#cursor").css("background", "url(img/mouse_arrow_windows_aero.png)");
     $("#cursor").animate({ left: dest.left, top: dest.top }, 1000 , "linear", typeStart);
   }
 };
