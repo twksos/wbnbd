ec87528428e8d25036a58b9d0663223d2962d5b0
Wei Guangcheng <twksos@qq.com>
2017-03-13 00:28:00 +0800

    make everything work

--- src/js/Searcher.js
+++ src/js/Searcher.js
@@ -7,13 +7,66 @@ export class Searcher {
     }
 
     copyText() {
-        copy(this.text);
+        if (this.playing) return;
+        copy('http://wbnbd.com/?q=' + this.text);
+        const shareHintDOM = document.querySelector('#share-hint');
+        shareHintDOM.innerHTML = '已复制';
     }
 
     search() {
-        const share = document.querySelector('#share');
-        Velocity(share, {opacity: 1}, {display: 'block'});
-        console.log(this.text);
+        if (this.playing) return;
+        const shareDOM = document.querySelector('#share');
+        const shareInputDOM = document.querySelector('#generated-url');
+        shareInputDOM.value = 'http://wbnbd.com/?q=' + this.text;
+        Velocity(shareDOM, {opacity: 1}, {display: 'block'});
+    }
+
+    play() {
+        const search = window.location.search;
+        const hash = window.location.hash;
+        if (!search.startsWith('?q='))return;
+        if (hash === '#ad') return;
+        this.playing = true;
+
+        const text = search.slice(3);
+        const navTextDOM = document.querySelector('#nv .welcome');
+        const cursorDOM = document.querySelector('#cursor');
+        const inputDOM = document.querySelector('#kw');
+        const searchButtonDOM = document.querySelector('#su');
+        const inputPosition = inputDOM.getBoundingClientRect();
+        const searchButtonPosition = searchButtonDOM.getBoundingClientRect();
+
+        navTextDOM.innerHTML = '想知道“'+ text + '”是什么？';
+        cursorDOM.style['background-image'] = 'url(/img/mouse_arrow_windows_aero.png)';
+        Velocity(cursorDOM, {left: inputPosition.left, top: inputPosition.top}, {
+            duration: 1500,
+            complete: function () {
+                cursorDOM.style['background-image'] = 'url(/img/mouse_text_windows_aero.png)';
+                navTextDOM.innerHTML = '只要在这里输入“'+ text + '”...';
+                setTimeout(function () {
+                    let textTyped = 0;
+                    const interval = setInterval(function () {
+                        textTyped += 1;
+                        inputDOM.value = text.slice(0, textTyped);
+                        if (textTyped === text.length) {
+                            clearInterval(interval);
+                            navTextDOM.innerHTML = '然后点右边那个按钮，简单吧~';
+                            cursorDOM.style['background-image'] = 'url(/img/mouse_arrow_windows_aero.png)';
+                            Velocity(cursorDOM, {
+                                left: searchButtonPosition.left + 5,
+                                top: searchButtonPosition.top + 5
+                            }, {
+                                duration: 1500,
+                                complete: function () {
+                                    window.location.hash = 'ad';
+                                    window.location.href = 'https://baidu.com/s?wd=' + text;
+                                }
+                            });
+                        }
+                    }, 300);
+                }, 300);
+            }
+        });
     }
 }
 Searcher.instance = null;
